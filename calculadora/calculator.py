4f8220142e8988c7a4f1e67150138322c06572e4
unknown <Administrador@UNI001-L008-018.serlab.intra>
2026-03-27 10:55:31 -0300

    Projeto finalizado com todas as operações e testes

--- calculadora/calculator.py
+++ calculadora/calculator.py
@@ -1,16 +1,14 @@
 class Calculator:
     def add(self, a: float, b: float) -> float:
-        """Metodo ja implementado para servir de exemplo."""
         return a + b
 
     def subtract(self, a: float, b: float) -> float:
-        # TODO(aluno): implementar subtracao.
-        raise NotImplementedError("Metodo subtract ainda nao foi implementado.")
+        return a - b
 
     def multiply(self, a: float, b: float) -> float:
-        # TODO(aluno): implementar multiplicacao.
-        raise NotImplementedError("Metodo multiply ainda nao foi implementado.")
+        return a * b
 
     def divide(self, a: float, b: float) -> float:
-        # TODO(aluno): implementar divisao e tratar divisao por zero.
-        raise NotImplementedError("Metodo divide ainda nao foi implementado.")
+        if b == 0:
+            raise ValueError("Divisão por zero não é permitida.")
+        return a / b
--- calculadora/tests.py
+++ calculadora/tests.py
@@ -1,11 +1,22 @@
 from django.test import TestCase
-
 from .calculator import Calculator
 
-
 class CalculatorTestCase(TestCase):
-    def test_add(self):
-        calc = Calculator()
-        self.assertEqual(calc.add(2, 3), 5)
+    def setUp(self):
+        self.calc = Calculator()
 
-    # TODO(aluno): adicionar testes para subtract, multiply e divide.
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 4), 6)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 5), 15)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
